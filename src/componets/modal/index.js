93120900668622e3be73762fcfeb6c901b62fb2c
jocimrjsc <jocimarjsc@yahoo.com.br>
2020-10-08 17:50:25 -0400

    update

--- src/componets/modal/index.js
+++ src/componets/modal/index.js
@@ -16,7 +16,7 @@ const Modal = ({ close, id = 'modal', monthActual, day }) => {
             <div id={id} onClick={handleOutsideClick} className="modal-backdrop">
                 <div className="modal-box">
                     <div className="modal-header">
-                        <h3>Cadastrar serviço</h3>
+                        <h3>Pedido arranjos de balões</h3>
                         <div className="date">
                             <input type="text" value={`${day}/${monthActual}/${getYear()}`} />
                         </div>
@@ -27,12 +27,58 @@ const Modal = ({ close, id = 'modal', monthActual, day }) => {
                     <div className="modal-content">
                         <form action="" method="post">
                             <div className="modal-left">
-                                <input type="text" placeholder='Nome' />
+                                <div className="modal-label-input">
+                                    <label htmlFor="name">Nome</label>
+                                    <input type="text" placeholder='Nome' />
+                                </div>
+
+                                <div className="modal-label-input">
+                                    <label htmlFor="phone">Nome</label>
+                                    <input type="text" placeholder='Telefone' />
+                                </div>
+
+                                <div className="modal-label-input">
+                                    <label htmlFor="address">Endereço</label>
+                                    <input type="text" placeholder='Endereço' />
+                                </div>
                             </div>
 
                             <div className="modal-right">
                                 <div className="modal-left">
-                                    <input type="text" placeholder='Nome' />
+                                    <div className="modal-label-input">
+                                        <label htmlFor="model">Modelo</label>
+                                        <div className="modal-check">
+                                            <div className="modal-check-radio">
+                                                <label htmlFor="balao-cake">Balão cake</label>
+                                                <input type='radio' id="balao-cake" value="balao-cake" name="balao" />
+                                            </div>
+
+                                            <div className="modal-check-radio">
+                                                <label htmlFor="balao-arranjo">Balão arranjo</label>
+                                                <input type='radio' id="balao-arranjo" value="balao-arranjo" name="balao" />
+                                            </div>
+
+                                            <div className="modal-check-radio">
+                                                <label htmlFor="somente-balao">Somente balão</label>
+                                                <input type='radio' id="somente-balao" value="somente-balao" name="balao" />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="modal-label-input">
+                                        <label htmlFor="color">Cores</label>
+                                        <input type="text" placeholder='Cores' />
+                                    </div>
+
+                                    <div className="modal-label-input">
+                                        <label htmlFor="written">Escrito no balão</label>
+                                        <input type="text" placeholder='Escrito no Balão' />
+                                    </div>
+
+                                    <div className="modal-label-input">
+                                        <label htmlFor="description">Descrição</label>
+                                        <textarea name="description" id="description" placeholder='descrição' />
+                                    </div>
                                 </div>
                             </div>
                         </form>
